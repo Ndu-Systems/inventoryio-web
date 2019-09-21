0c0f924e6fc1c5926a81b096a1bca25ccd18088d
Freedom Khanyile <Freedom.Khanyile@betterlife.co.za>
2019-09-21 17:11:28 +0200

    change css to scss

--- src/app/views/dashboard/users/list-roles/list-roles.component.ts
+++ src/app/views/dashboard/users/list-roles/list-roles.component.ts
@@ -3,7 +3,7 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-list-roles',
   templateUrl: './list-roles.component.html',
-  styleUrls: ['./list-roles.component.css']
+  styleUrls: ['./list-roles.component.scss']
 })
 export class ListRolesComponent implements OnInit {
 
