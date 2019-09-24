463cbb464f1a70b24a772c15b59d99597fe0adf5
Ndumiso Mthembu <Ndumiso.Mthembu@dimensiondata.com>
2019-09-24 18:33:21 +0200

    print the invoice

--- src/app/views/dashboard/products/invoice/invoice.component.ts
+++ src/app/views/dashboard/products/invoice/invoice.component.ts
@@ -1,15 +1,48 @@
 import { Component, OnInit } from '@angular/core';
+import { OrdersService } from 'src/app/_services';
+import { Observable } from 'rxjs';
+import { OrderProducts } from 'src/app/_models';
+import * as jspdf from 'jspdf';
+import html2canvas from 'html2canvas';
 
 @Component({
   selector: 'app-invoice',
   templateUrl: './invoice.component.html',
-  styleUrls: ['./invoice.component.css']
+  styleUrls: ['./invoice.component.scss']
 })
 export class InvoiceComponent implements OnInit {
+  products$: Observable<OrderProducts[]>;
+  companyTax = 0.1;
+  total = 0;
+  InvoiceDate: string;
+  InvoiceNumber: string;
 
-  constructor() { }
+
+  constructor(private ordersService: OrdersService,
+  ) { }
 
   ngOnInit() {
-  }
+    this.products$ = this.ordersService.orderProducts;
 
+    this.ordersService.order.subscribe(state => {
+      if (!state) { return; }
+      this.ordersService.getProductsForAnOrder(state.OrdersId);
+      this.total = state.Total;
+      this.InvoiceDate = state.CreateDate;
+      this.InvoiceNumber = state.OrderId;
+    });
+
+  }
+  public captureScreen() {
+    const data = document.getElementById('contentToConvert');
+    html2canvas(data).then(canvas => {
+      const imgWidth = 208;
+      const imgHeight = canvas.height * imgWidth / canvas.width;
+      const contentDataURL = canvas.toDataURL('image/png');
+      const pdf = new jspdf('p', 'mm', 'a4'); // A4 size page of PDF
+      const position = 0;
+      pdf.addImage(contentDataURL, 'PNG', 0, position, imgWidth, imgHeight);
+      pdf.save(`${this.InvoiceNumber} INVOICE.pdf`);
+    });
+  }
 }
--- src/app/views/dashboard/products/order-details/order-details.component.ts
+++ src/app/views/dashboard/products/order-details/order-details.component.ts
@@ -3,8 +3,7 @@ import { Observable } from 'rxjs';
 import { Orders, OrderProducts } from 'src/app/_models';
 import { OrdersService, AccountService, BannerService } from 'src/app/_services';
 import { Router } from '@angular/router';
-import * as jspdf from 'jspdf';
-import html2canvas from 'html2canvas';
+
 
 @Component({
   selector: 'app-order-details',
@@ -52,17 +51,5 @@ export class OrderDetailsComponent implements OnInit {
     this.ordersService.updateOrderState(order);
     this.ordersService.getProductsForAnOrder(order.OrdersId);
     this.router.navigate([`/dashboard/order-details`]);
-  }
-  public captureScreen() {
-    const data = document.getElementById('contentToConvert');
-    html2canvas(data).then(canvas => {
-      const imgWidth = 208;
-      const imgHeight = canvas.height * imgWidth / canvas.width;
-      const contentDataURL = canvas.toDataURL('image/png');
-      const pdf = new jspdf('p', 'mm', 'a4'); // A4 size page of PDF
-      const position = 0;
-      pdf.addImage(contentDataURL, 'PNG', 0, position, imgWidth, imgHeight);
-      pdf.save('MYPdf.pdf');
-    });
-  }
+  } 
 }
