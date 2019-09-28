812832f9f0cc835e3b43a5f9c109342514d0c7c1
Freedom Khanyile <Freedom.Khanyile@betterlife.co.za>
2019-09-28 10:10:08 +0200

    created role permissions system constants

---
+++ src/app/views/dashboard/shared/roles-permissions.ts
@@ -0,0 +1,44 @@
+// products permissions
+export const ProductPermissions = {
+  CAN_READ_PRODUCTS: {
+    key: 'CAN_READ_PRODUCTS',
+    desc: 'allows the user to view products on the system'
+  },
+  CAN_SELL_PRODUCTS: {
+    key: 'CAN_SELL_PRODUCTS',
+    desc: 'allows the user to sell products on the system'
+  },
+  CAN_ORDER_PRODUCTS: {
+    key: 'CAN_SELL_PRODUCTS',
+    desc: 'allows the user to order products on the system'
+  },
+  CAN_ADD_PRODUCTS: {
+    key: 'CAN_SELL_PRODUCTS',
+    desc: 'allows the user to add products on the system'
+  }
+};
+
+
+// orders permissions
+export const OrderPermissions = {
+  CAN_READ_ORDERS: {
+    key: 'CAN_READ_ORDERS',
+    desc: 'allows the user to view orders on the system'
+  },
+  CAN_PAY_ORDERS: {
+    key: 'CAN_PAY_ORDERS',
+    desc: 'allows the user to view orders on the system'
+  },
+  CAN_PRINT_ORDERS: {
+    key: 'CAN_PRINT_ORDERS',
+    desc: 'allows the user to view orders on the system'
+  }
+};
+
+export const ConfigurationPermissions = {
+  CAN_CONFIGURE: {
+    key: 'CAN_CONFIGURE',
+    desc: 'allows the user to to change the configurations on the system'
+  }
+};
+
