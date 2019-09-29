fac7e90b38af9d178d71199b819a9136557574d3
Ndumiso Mthembu <Ndumiso.Mthembu@dimensiondata.com>
2019-09-29 18:44:35 +0200

    sell validations

--- src/app/views/dashboard/products/sell/sale-sammary/sale-sammary.component.ts
+++ src/app/views/dashboard/products/sell/sale-sammary/sale-sammary.component.ts
@@ -1,7 +1,8 @@
 import { Component, OnInit } from '@angular/core';
-import { SaleService } from 'src/app/_services';
+import { SaleService, ProductService } from 'src/app/_services';
 import { Observable } from 'rxjs';
-import { SellModel, Item } from 'src/app/_models';
+import { SellModel, Item, Product } from 'src/app/_models';
+import { MessageService } from 'primeng/api';
 
 @Component({
   selector: 'app-sale-sammary',
@@ -10,35 +11,63 @@ import { SellModel, Item } from 'src/app/_models';
 })
 export class SaleSammaryComponent implements OnInit {
   sale$: Observable<SellModel>;
-  constructor(private saleService: SaleService) { }
+  products: Product[];
+  constructor(
+    private saleService: SaleService,
+    private messageService: MessageService,
+    private productService: ProductService,
+  ) { }
 
   ngOnInit() {
     this.sale$ = this.saleService.sell;
     this.saleService.sell.subscribe(state => {
       // alert(state);
     });
+    this.productService.products.subscribe(r => {
+      this.products = r;
+    });
   }
 
   add(item: Item) {
+    const product = this.products.find(x => x.ProductId === item.prodcuId);
+    if (Number(product.Quantity) - Number(item.quantity) <= 0) {
+      this.messageService.add({
+        severity: 'warn',
+        summary: 'Stock Alert.',
+        detail: `You run out of ${product.Name}`
+      });
+      return false;
+    }
     item.quantity++;
     this.saleService.doSellLogic(item);
   }
-  reduce(item: Item) {
-    if (item.quantity <= 0) {
-      this.saleService.removeItem(item);
-      return;
+    reduce(item: Item) {
+      if (item.quantity <= 0) {
+        this.saleService.removeItem(item);
+        return;
+      }
+      item.quantity--;
+      this.saleService.doSellLogic(item);
     }
-    item.quantity--;
-    this.saleService.doSellLogic(item);
-  }
-  close(item: Item) {
-    this.saleService.removeItem(item);
-  }
-  blur(item: Item) {
-    if (item.quantity <= 0) {
+    close(item: Item) {
       this.saleService.removeItem(item);
-      return;
     }
-    this.saleService.doSellLogic(item);
+    blur(item: Item) {
+      if (item.quantity <= 0) {
+        this.saleService.removeItem(item);
+        return;
+      }
+
+      const product = this.products.find(x => x.ProductId === item.prodcuId);
+      if (Number(product.Quantity) - Number(item.quantity) <= 0) {
+        this.messageService.add({
+          severity: 'warn',
+          summary: 'Stock Alert.',
+          detail: `You run out of ${product.Name}`
+        });
+        item.quantity = product.Quantity;
+        return false;
+      }
+      this.saleService.doSellLogic(item);
+    }
   }
-}
