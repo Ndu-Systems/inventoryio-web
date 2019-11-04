4959aac02b54f0aa2a1bed20ae12a31fe929d1c3
Ndumiso Mthembu <Ndumiso.Mthembu@dimensiondata.com>
2019-11-04 07:15:18 +0200

    upload size minimzed

--- src/app/views/dashboard/products/product-images/product-images.component.ts
+++ src/app/views/dashboard/products/product-images/product-images.component.ts
@@ -64,9 +64,10 @@ export class ProductImagesComponent implements OnInit {
     }
 
     Array.from(this.files).forEach(file => {
-      this.documentsService.uploadFile(file).subscribe(response => {
-        this.saveImage(response);
-      });
+      // this.documentsService.uploadFile(file).subscribe(response => {
+      //   this.saveImage(response);
+      // });
+      this.cropImage(file);
     });
 
   }
@@ -86,5 +87,79 @@ export class ProductImagesComponent implements OnInit {
       summary: 'Success!',
       detail: 'Image  uploaded '
     });
+  }
+  cropImage(file) {
+    if (file.type.match(/image.*/)) {
+      console.log('An image has been loaded');
+
+      const reader = new FileReader();
+      reader.onload = (readerEvent: any) => {
+        const image = new Image();
+        image.onload = (imageEvent) => {
+
+          // Resize the image
+          const canvas = document.createElement('canvas');
+          const maxSize = 544; // TODO : pull max size from a site config
+          let width = image.width;
+          let height = image.height;
+          if (width > height) {
+            if (width > maxSize) {
+              height *= maxSize / width;
+              width = maxSize;
+            }
+          } else {
+            if (height > maxSize) {
+              width *= maxSize / height;
+              height = maxSize;
+            }
+          }
+          canvas.width = width;
+          canvas.height = height;
+          canvas.getContext('2d').drawImage(image, 0, 0, width, height);
+          const dataUrl = canvas.toDataURL('image/jpeg');
+          const resizedImage = this.dataURLToBlob(dataUrl);
+          let fileOfBlob = new File([resizedImage], 'iio.jpg');
+          // upload
+          let formData = new FormData();
+          formData.append('file', fileOfBlob);
+          formData.append('name', 'iio');
+          this.documentsService.uploadFile(formData).subscribe(response => {
+            //  this.imgURL.push(`${environment.API_URL}/api/upload/${response}`);
+            // this.uploadService.updateState(this.imgURL);
+            this.saveImage(response);
+            console.log(response);
+          });
+
+        };
+        image.src = readerEvent.target.result.toString();
+      };
+      reader.readAsDataURL(file);
+    }
+  }
+  dataURLToBlob(dataURL) {
+    const BASE64_MARKER = ';base64,';
+    if (dataURL.indexOf(BASE64_MARKER) === -1) {
+      // tslint:disable-next-line: no-shadowed-variable
+      const parts = dataURL.split(',');
+      // tslint:disable-next-line: no-shadowed-variable
+      const contentType = parts[0].split(':')[1];
+      // tslint:disable-next-line: no-shadowed-variable
+      const raw = parts[1];
+
+      return new Blob([raw], { type: contentType });
+    }
+
+    const parts = dataURL.split(BASE64_MARKER);
+    const contentType = parts[0].split(':')[1];
+    const raw = window.atob(parts[1]);
+    const rawLength = raw.length;
+
+    const uInt8Array = new Uint8Array(rawLength);
+
+    for (let i = 0; i < rawLength; ++i) {
+      uInt8Array[i] = raw.charCodeAt(i);
+    }
+
+    return new Blob([uInt8Array], { type: contentType });
   }
 }
