4e3871b7e91cbe8900cb83839621a8c0d61c6604
Freedom Khanyile <Freedom.Khanyile@betterlife.co.za>
2019-12-07 20:36:49 +0200

    navigation bar component

--- src/app/views/home/home-routing.module.ts
+++ src/app/views/home/home-routing.module.ts
@@ -1,28 +1,34 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { HomeComponent } from './home.component';
-import { SignInComponent } from './accounts/sign-in/sign-in.component';
-import { SignUpComponent } from './accounts/sign-up/sign-up.component';
-import { IndexComponent } from './index/index.component';
-import { HomePageComponent } from './index/home-page/home-page.component';
-import { FeaturesComponent } from './index/features/features.component';
 import { SplashHomeComponent } from '../dashboard/shared/splash-home/splash-home.component';
 import { PricingComponent } from './index/Pricing/Pricing.component';
+import { HomeNavComponent } from './shared/home-nav';
+import { IndexComponent, HomePageComponent, FeaturesComponent } from './index';
+import { SignUpComponent, SignInComponent } from './accounts';
 const routes: Routes = [
   {
     path: '', component: HomeComponent,
     children: [
       { path: 'home', component: IndexComponent },
       { path: '', component: SignInComponent },
-      { path: 'Pricing',component: PricingComponent},
+      { path: 'Pricing', component: PricingComponent },
       { path: 'sign-in', component: SignInComponent },
       { path: 'sign-up', component: SignUpComponent }]
   }
 ];
 
-export const declarations = [SignInComponent,
-  HomeComponent, SignUpComponent, IndexComponent,
-  HomePageComponent, FeaturesComponent,SplashHomeComponent,PricingComponent];
+export const declarations = [
+  SignInComponent,
+  SignUpComponent,
+  HomeComponent,
+  IndexComponent,
+  HomePageComponent,
+  FeaturesComponent,
+  SplashHomeComponent,
+  PricingComponent,
+  HomeNavComponent
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
