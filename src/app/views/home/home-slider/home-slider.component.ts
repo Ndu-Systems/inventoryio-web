f1c42988fcf2e7d796c3457be408519611ef5214
Ndumiso Mthembu <Ndumiso.Mthembu@dimensiondata.com>
2019-12-12 20:54:04 +0200

    nice slader

--- src/app/views/home/home-slider/home-slider.component.ts
+++ src/app/views/home/home-slider/home-slider.component.ts
@@ -6,30 +6,37 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./home-slider.component.scss']
 })
 export class HomeSliderComponent implements OnInit {
+  slideClass = ['col-1-of-2', 'hidded-slide'];
   slides: Slide[] = [
     {
       heading: `Welcome to Inventory IO`,
       paragraph: `Say goodbye to the paper process and discover intelligent inventory management and sales.`,
       img: `assets/images/home/silder/one.png`,
-      active: true
+      active: true,
+      slideClass: [...this.slideClass, 'visibile-slide']
     },
     {
       heading: `Welcome to Inventory IO`,
       paragraph: `Create product inventory of any type or make it's secured data driven. Which
                   means you can modify the information that matters to you.`,
       img: `assets/images/home/silder/two.png`,
-      active: false
+      active: false,
+      slideClass: this.slideClass
+
     },
     {
       heading: `Welcome to Inventory IO`,
       paragraph: `Manage your sales, product inventory with insights for your business that keeps you up to updated.`,
       img: `assets/images/home/silder/three.png`,
-      active: false
+      active: false,
+      slideClass: this.slideClass
+
     },
   ];
   constructor() { }
 
   ngOnInit() {
+    // this.slides[0].slideClass.push('visibile-slide');
     this.playSlide();
   }
   playSlide() {
@@ -38,10 +45,19 @@ export class HomeSliderComponent implements OnInit {
       if (slide.active) {
         const index = this.slides.indexOf(slide);
         slide.active = false;
+        slide.slideClass = this.slideClass;
         if (index < this.slides.length - 1) {
-          if (this.slides[index + 1]) { this.slides[index + 1].active = true; }
+          if (this.slides[index + 1]) {
+            this.slides[index + 1].active = true;
+            this.slides[index + 1].slideClass = [...this.slideClass, 'visibile-slide'];
+         // this.slides[index + 1].slideClass = [...this.slideClass, 'visibile-slide'l;
+
+          }
         } else {
-          if (this.slides[0]) { this.slides[0].active = true };
+          if (this.slides[0]) {
+            this.slides[0].active = true;
+            this.slides[0].slideClass = [...this.slideClass, 'visibile-slide'];
+          }
         }
       }
       // this.slides.forEach((slide, index) => {
@@ -67,4 +83,5 @@ export interface Slide {
   paragraph: string;
   img: string;
   active: boolean;
+  slideClass: string[];
 }
