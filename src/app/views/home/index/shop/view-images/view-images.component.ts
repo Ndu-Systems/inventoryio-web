6a280cc7da22aee04fdd6b3bcb92f489bf4da59e
Ndumiso Mthembu <Ndumiso.Mthembu@dimensiondata.com>
2020-03-05 20:46:50 +0200

    view images done

--- src/app/views/home/index/shop/view-images/view-images.component.ts
+++ src/app/views/home/index/shop/view-images/view-images.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { ProductService } from 'src/app/_services';
 import { Observable } from 'rxjs';
-import { Product } from 'src/app/_models';
+import { Product, Image } from 'src/app/_models';
 
 @Component({
   selector: 'app-view-images',
@@ -10,18 +10,21 @@ import { Product } from 'src/app/_models';
 })
 export class ViewImagesComponent implements OnInit {
   product$: Observable<Product>;
-  images: any[] = [];
+  images: Image[] = [];
+  selectedImage: Image;
   constructor(private productService: ProductService
   ) { }
 
   ngOnInit() {
     this.product$ = this.productService.sellItem;
     this.productService.sellItem.subscribe(data => {
-      data.images.forEach(image => {
-        this.images.push({ source: image.Url, alt: '', title: '' });
-      });
+      this.images = data.images;
+      this.selectedImage = this.images[0];
     });
 
+  }
+  selectImage(image: Image) {
+    this.selectedImage = image;
   }
 
 }
