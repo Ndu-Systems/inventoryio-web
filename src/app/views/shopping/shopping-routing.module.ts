5b75e38459cf43bfad6055ecebd8307c01ba2184
Ndumiso Mthembu <Ndumiso.Mthembu@dimensiondata.com>
2020-03-09 07:49:14 +0200

    fixing shopping components folder structure

--- src/app/views/shopping/shopping-routing.module.ts
+++ src/app/views/shopping/shopping-routing.module.ts
@@ -2,13 +2,13 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { FormatOptionsPipe } from 'src/app/_pipes/format-options.pipe';
 import { ShopsComponent } from './components/shop/shops/shops.component';
-import { ShopComponent } from './components/shop/shop.component';
 import { ShoppingCartComponent } from './components/shop/shopping-cart/shopping-cart.component';
 import { ShopingSuccesfulComponent } from './components/shop/shoping-succesful/shoping-succesful.component';
 import { ViewProductComponent } from './components/shop/view-product/view-product.component';
-import { ShoppingComponent } from './shopping.component';
 import { ShippingComponent } from './components/shop/shipping/shipping.component';
 import { ViewImagesComponent } from './components/shop/view-images/view-images.component';
+import { ShoppingComponent } from './components/shop/shopping/shopping.component';
+import { ShopComponent } from './components/shop/shop/shop.component';
 const routes: Routes = [
   {
     path: '', component: ShoppingComponent,
